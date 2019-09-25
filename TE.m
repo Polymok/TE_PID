24b51c736d3de597240e7448ecd92b590b28236c
Mofei Wu <mwumofei@gmail.com>
2019-09-25 14:23:47 -0500

    Section comments

--- TE.m
+++ TE.m
@@ -8,26 +8,28 @@
 % This function can only take discrete time-series.
 
 function [transfer_entropy, normed_TE] = TE(target, source, delay)
-    % Ensure input time-series are column vectors.
-%     if size(target,1) < size(target,2)
-%         str = input('Target input vector has a greater number of columns than rows. Each column should contain the entire time-series of a single neuron. Transpose input matrix? y/n: ','s');
-%         if str == 'y'
-%             target = target';
-%         end
-%     end
-%     if size(source,1) < size(source,2)
-%         str = input('Source input vector has a greater number of columns than rows. Each column should contain the entire time-series of a single neuron. Transpose input matrix? y/n: ','s');
-%         if str == 'y'
-%             source = source';
-%         end
-%     end
+
+    %% Check inputs.
+    if size(target,1) < size(target,2)
+        str = input('Target input vector has a greater number of columns than rows. Each column should contain the entire time-series of a single neuron. Transpose input matrix? y/n: ','s');
+        if str == 'y'
+            target = target';
+        end
+    end
+    if size(source,1) < size(source,2)
+        str = input('Source input vector has a greater number of columns than rows. Each column should contain the entire time-series of a single neuron. Transpose input matrix? y/n: ','s');
+        if str == 'y'
+            source = source';
+        end
+    end
     % Check if inputs are of the same length.
-%     if size(target,1)~= size(source,1)
-%         error('Time-series are not of equal length.')
-%     elseif (round(delay)~=delay) || (delay<1)
-%         error('Input time-delay is not a positive integer.')
-%     end
-    % Truncate at beginning or end of time-series to create time-delay.
+    if size(target,1)~= size(source,1)
+        error('Time-series are not of equal length.')
+    elseif (round(delay)~=delay) || (delay<1)
+        error('Input time-delay is not a positive integer.')
+    end
+    
+    %% Truncate at beginning or end of time-series to create time-delay.
     target_future = target;
     target_future(1:delay,:) = [];
     target_past = target;
@@ -35,8 +37,10 @@ function [transfer_entropy, normed_TE] = TE(target, source, delay)
     source_past = source;
     source_past((size(source,1)-delay+1):size(source,1),:) = [];
     clear target source;
+    
+    %% Calculate transfer entropy.
     transfer_entropy = cond_MI(source_past, target_future, target_past);
-    % Normalize by entropy of target time-series.
+    % Optionally, normalize by entropy of target time-series.
     if nargout==2
         target_entropy = 0;
         for i = unique(target_future, 'row')'
@@ -44,7 +48,7 @@ function [transfer_entropy, normed_TE] = TE(target, source, delay)
             target_entropy = target_entropy - prob * log2(prob);
         end
         if target_entropy == 0
-%             disp('Target time-series has zero entropy. Using unnormalized transfer entropy.')
+            disp('Target time-series has zero entropy. Using unnormalized transfer entropy.')
           normed_TE = transfer_entropy;
         else
             normed_TE = transfer_entropy / target_entropy;
